c964a4e92070ba0ad233369738f3d24719e50b06
Margo1212 <sirachenko944@gmail.com>
2023-12-04 19:14:52 +0100

    add images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,24 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../frontend/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "../frontend/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    colors: {
+      "main-bg": "#180022",
+      white: "#fff",
+      primary: "#EE0CF2",
+      secondary: "#36FFB5",
+    },
     extend: {},
   },
   plugins: [],
