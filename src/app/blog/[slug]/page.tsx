fe11bc8b0c3b87568917fd4bf13e6393a1140aea
Margo1212 <marharytalesniak@gmail.com>
2024-01-04 13:49:04 +0100

    add post page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,41 @@
+
+
+import { getPostBySlug } from "@lib/data/posts";
+import Image from "next/image";
+
+
+
+// export const revalidate = 9;
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const postData = getPostBySlug({ slug: params.slug });
+  const post: any = await Promise.resolve(postData).catch((err) =>
+    console.error(err)
+  );
+
+  console.log(post)
+
+  return (
+    <div className="relative flex gap-4  px-6 py-4 laptop:py-16 laptop:px-20 bg-main-bg overflow-clip">
+      
+      <div className="laptop:w-1/2 space-y-5">
+        <div className="space-y-5">
+          <h2 className="text-2xl text-white font-normal text-center laptop:text-4xl laptop:text-left ">
+            {post.title}
+          </h2>
+       
+          <p className="laptop:text-base text-sm text-text">{post.description}</p>
+        </div>
+
+      {post.textblock?.map((text: any) => (text.type === 'heading' ? <div key={text.id}>{text.children.map((p: any) => (<h4 key={p.id} className="font-medium text-white text-xl">{p.text}</h4>))}</div> : <div key={text.id}>{text.children.map((p: any) => (<p className="font-normal text-text text-base" key={p.id}>{p.text}</p>))}</div>
+          ))}
+        
+      </div>
+      <div className="laptop:w-2/5 h-1/2 w-full px-10 py-5 flex justify-center">
+        {post.image?.data !== null ? (
+          <Image src={post?.image?.data[0]?.attributes?.url} alt={post?.image?.data[0]?.attributes?.alternativeText} width={post?.image?.data[0]?.attributes?.width} height={post?.image?.data[0]?.attributes?.height}/>
+        ) : null}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,75 @@
+import { NextBreadcrumb } from "@lib/components/Breadcrumbs/Breadcrumbs";
+import { Title } from "@lib/components/Title/Title";
+import { getPosts } from "@lib/data/posts";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default async function Page() {
+  const postData = getPosts();
+  const posts = await Promise.resolve(postData).catch((err) =>
+    console.error(err)
+  );
+
+  return (
+    <section className="bg-main-bg px-5 laptop:px-24 gap-3 pt-10 pb-32 desktop:px-44 space-y-10">
+      <Title>Blog</Title>
+      <NextBreadcrumb
+        homeElement={"Strona Główna"}
+        separator={
+          <svg
+            className="w-6 h-6 text-text"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        }
+        activeClasses="text-amber-500"
+        containerClasses="flex py-5 bg-gradient-to-r from-purple-600 to-blue-600"
+        listClasses="hover:underline mx-2 font-bold text-text"
+        capitalizeLinks
+      />
+      <div className="grid laptop:grid-cols-3 laptop:gap-x-14 content-center tablet:grid-cols-2 tablet:gap-x-12 grid-cols-1 desktop:gap-x-32 gap-y-14">
+        {posts.map((post: any) => (
+          <div key={post.id} className="flex flex-col gap-y-5">
+            <div className="w-full h-[250px]">
+              <Image
+                className="rounded-lg h-full"
+                src={post?.image.data[0]?.attributes?.url}
+                alt={post?.image.data[0]?.attributes?.alternativeText}
+                width={post?.image.data[0]?.attributes?.width}
+                height={post?.image.data[0]?.attributes?.height}
+              />
+            </div>
+            <div className="w-full flex flex-col gap-y-5">
+              <p className="font-medium text-text text-sm">
+                {post.createdat} - {post.author.data.attributes.name}
+              </p>
+              <h4 className="font-medium text-white text-3xl">{post.title}</h4>
+              <p className="font-normal text-text text-base">
+                {post.description}
+              </p>
+              <Link
+                href={`/blog/${post.slug}`}
+                className="text-secondary uppercase"
+              >
+                Czytaj Więcej
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
