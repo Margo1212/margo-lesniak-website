30c486057fc2254da1b8bfd0e98c46c6261bc694
Margo1212 <marharytalesniak@gmail.com>
2024-02-20 21:30:58 +0100

    update cookies

--- app/ui/components/Cookies/Cookies.tsx
+++ app/ui/components/Cookies/Cookies.tsx
@@ -22,10 +22,10 @@ const CookieConsent = () => {
 
   return (
     <div className="relative w-full z-50 flex justify-start">
-      <div className="fixed bottom-44 tablet:bottom-5 rounded-3xl shadow-[5px_5px_9px_#131519,-5px_-5px_9px_#272d35]  bg-main-bg w-full tablet:w-[60%] mb-30 flex items-center justify-between gap-x-5 px-4 py-2 laptop:py-4">
+      <div className="fixed   bottom-48 px-10 py-10 tablet:bottom-5 rounded-3xl shadow-[5px_5px_9px_#131519,-5px_-5px_9px_#272d35]  bg-main-bg  tablet:w-[450px] mb-30 flex flex-col  items-center gap-y-5 tablet:mx-0 mx-5 justify-between tablet:gap-x-5 tablet:px-4 tablet:py-2 laptop:py-4">
         <CookieOutlinedIcon fontSize="large" sx={{ color: "#36FFB5" }} />
-        <span className="text-white text-xs tablet:text-sm laptop:text-base my-5 laptop:my-5">
-          Używamy plików cookie na naszej stronie internetowej, aby zapewnić Ci
+        <span className="text-white text-sm laptop:text-base my-5 laptop:my-5">
+          Używam plików cookie na naszej stronie internetowej, aby zapewnić Ci
           najbardziej odpowiednie preferencje. Klikając „Akceptuj”, wyrażasz
           zgodę na użycie plików cookie.
         </span>
